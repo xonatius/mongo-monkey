cfd065506ba643480e966df559b163359170979c
Daniil Bondarev <xonatius@gmail.com>
2012-11-20 00:00:08 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+
+setuptools.setup(
+    name="Mongo Monkey",
+    version="0.0.1",
+    author="Daniil Bondarev",
+    author_email="xonatius@gmail.com",
+    description="A MongoDB object-document mapping layer for Python",
+    license="BSD",
+    keywords="mongo mongodb database pymongo odm",
+    url="http://github.com/xonatius/mongo-monkey",
+    packages=["mongomonkey"],
+    long_description="Mongo-monkey is a MongoDB object-document mapping API for Python.",
+    install_requires=['pymongo'],
+    tests_require=['nose']
+    )
